bf8c8b60a571da4b8bd742bd562c2246af04d36c
Dawid Zysk <dawid.zysk@student.uj.edu.pl>
2021-03-07 00:22:24 +0100

    char* arrays

--- Surname/Source.cpp
+++ Surname/Source.cpp
@@ -14,6 +14,7 @@ int main()
 		std::string* arrayS = new std::string[n];
 		for (int i = 0; i < n; i++)
 		{
+			std::cout << "Write next surname" << std::endl;
 			std::cin >> *(arrayS + i);
 		}
 		for (int i = 0; i < n; i++)
@@ -24,6 +25,20 @@ int main()
 	}
 	else
 	{
-
+		int length;
+		char** arrayC = new char* [n];
+		for (int i = 0; i < n; i++)
+		{
+			std::cout << "How many characters does your next surname have?" << std::endl;
+			std::cin >> length;
+			arrayC[i] = new char[length];
+			std::cout << "Write next surname" << std::endl;
+			std::cin >> arrayC[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			delete[] arrayC[i];
+		}
+		delete[] arrayC;
 	}
 }
